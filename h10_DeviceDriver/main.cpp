b169bfc7cc914cceb2962a5da8f939b778c30f8b
wanny <wankims@gmail.com>
2025-04-15 10:10:36 +0900

    add read success case

--- h10_DeviceDriver/main.cpp
+++ h10_DeviceDriver/main.cpp
@@ -15,35 +15,51 @@ public:
 class DeviceDriverFixture : public Test {
 public:
 	MockFlashDevice mock;
+	const int NEED_READ_TIMES = 5;
 };
 
 TEST_F(DeviceDriverFixture, ReadFromHW5TimeSuccess) {
-
+	//arrange
 	EXPECT_CALL(mock, read(0xBB))
-		.Times(5);
+		.Times(NEED_READ_TIMES);
 
 	DeviceDriver driver{ &mock };
+	//act
 	int data = driver.read(0xBB);
-
+	//assert
 	EXPECT_EQ(0, data);
 }
 
 TEST_F(DeviceDriverFixture, ReadFromHW5TimeFail) {
-
+	//arrange
 	EXPECT_CALL(mock, read)
 		.WillOnce(Return(0))
 		.WillRepeatedly(Return(5));
 
 	DeviceDriver driver{ &mock };
-	//act
 	try {
+		//act
 		int data = driver.read(0xBB);
 		FAIL();
 	}
 	catch (const ReadFailException& e) {
+		//assert
 		EXPECT_EQ(string{ e.what() }, string{ "ReadFailException" });
 	}
 }
+
+TEST_F(DeviceDriverFixture, ReadFromHW5TimeValueSuccess) {
+	//arrange
+	EXPECT_CALL(mock, read)
+		.Times(NEED_READ_TIMES)
+		.WillRepeatedly(Return(0));
+
+	DeviceDriver driver{ &mock };
+	int data = driver.read(0xBB);
+	//assert
+	EXPECT_EQ(0, data);
+}
+
 
 int main()
 {
